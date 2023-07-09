e23487c359720803ac5cf78808b4c4315605b220
Leandro Santos <lsaantos@outlook.com>
2023-07-09 20:30:43 -0300

    feat: change layout to column on mobile to improve responsivity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { CharactersList } from '~/components/CharactersList';
 import type { Character } from '~/types';
 
 const ContentWrapper = ({ children }: { children: ReactNode }) => (
-  <span className="flex flex-1 justify-center transition-all xl:max-w-[40%]">
+  <span className="flex flex-1 self-stretch justify-center transition-all lg:self-auto xl:max-w-[40%]">
     {children}
   </span>
 );
@@ -15,7 +15,7 @@ export default function Home() {
   const [selectedCharacter, setSelectedCharacter] = useState<Character>();
 
   return (
-    <div className="flex flex-1 items-center justify-center gap-12 transition-all 2xl:-mt-[120px]">
+    <div className="flex flex-col flex-1 items-center justify-center gap-12 transition-all 2xl:-mt-[120px] lg:flex-row">
       <ContentWrapper>
         <CharactersList
           selectedCharacter={selectedCharacter}
